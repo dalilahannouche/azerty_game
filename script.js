b89d5074e85872a398337576cac7bb8631ac4cc6
dalilahannouche <dalilahannouche@hotmail.fr>
2024-04-27 13:38:19 +0300

    nouvelle fonction lancer jeu

--- script.js
+++ script.js
@@ -12,39 +12,25 @@ let index_mots = 0
 let index_phrases = 0
 let score = 0
 
-affichage_dynamique_liste.innerHTML = listes_mots[index_mots]
 
-liste_mots.addEventListener('click', ()=> {
-    
-    if (index_mots < listes_mots.length){
-        affichage_dynamique_liste.innerHTML = listes_mots[index_mots]
-        index_mots++ 
-    } else{
-        affichage_dynamique_liste.innerHTML = "Le jeu est fini"
-    }
-    
-    
-})
+function lancerJeu(){  
+    affichage_dynamique_liste.innerHTML = listes_mots[index_mots] 
+    console.log(affichage_dynamique_liste.innerHTML)
 
-
-liste_phrases.addEventListener('click', ()=> {
-    
-    if (index_phrases < listes_phrases.length){
-        affichage_dynamique_liste.innerHTML = listes_phrases[index_phrases]
-        index_phrases++
-    } else{
-        affichage_dynamique_liste.innerHTML = "Le jeu est fini"
-    }
-    
-    
-})
+    bouton_validation.addEventListener('click', ()=>{
+        if (mot_utilisateur.value === affichage_dynamique_liste.innerHTML){
+            score++
+            mot_utilisateur.value = " "
+            score_change.textContent = score 
+            list_change.textContent = listes_mots.length
+            index_mots++ 
+            affichage_dynamique_liste.innerHTML = listes_mots[index_mots] 
+        }
+        else{
+            affichage_dynamique_liste.innerHTML = "Le jeu est fini"
+           }
+    }) 
+      
+}
 
 
-bouton_validation.addEventListener('click', ()=>{
-    if (mot_utilisateur.value === affichage_dynamique_liste.innerHTML){
-        score++
-        mot_utilisateur.value = " "
-        score_change.textContent = score 
-        list_change.textContent = listes_mots.length
-    }
-})
