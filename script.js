f044209aaca622cada5eba93501a003b1262079d
dalilahannouche <dalilahannouche@hotmail.fr>
2024-04-28 13:07:15 +0300

    pour les mots et pour les phrases
    Beaucoup de modifications dans le code pour incorporer une fonction qui inclue la liste des mots et phrases au choix de l'utilisateur

--- script.js
+++ script.js
@@ -7,44 +7,49 @@ const mot_utilisateur = document.querySelector(".validation input")
 const bouton_validation = document.querySelector(".validate")
 let score_change = document.querySelector(".score")
 let list_change = document.querySelector(".liste")
-
 let index_mots = 0
 let index_phrases = 0
+let index_list = 0
+
 let score = 0
 
+function listeChoix(listeToChoose, index_list){
+    listeToChoose.addEventListener('click', ()=>{
+        affichage_dynamique_liste.innerHTML = listeToChoose[index_list]
+        bouton_validation.addEventListener('click', ()=>{
+            if (mot_utilisateur.value === affichage_dynamique_liste.innerHTML){
+                score++
+                score_change.textContent = score 
+                list_change.textContent = listeToChoose.length
+                index_list++ 
+                if (index_list < listeToChoose.length) {
+                    affichage_dynamique_liste.innerHTML = listeToChoose[index_list];
+                } else {
+                    affichage_dynamique_liste.innerHTML = "Le jeu est fini";
+                    mot_utilisateur.disabled = true;
+                }
+                mot_utilisateur.value = ""
+            }
+    
+            else{
+                index_list++ 
+                if (index_list < listeToChoose.length) {
+                    affichage_dynamique_liste.innerHTML = listeToChoose[index_list];
+                } else {
+                    affichage_dynamique_liste.innerHTML = "Le jeu est fini";
+                    mot_utilisateur.disabled = true;
+                }
+                mot_utilisateur.value = "" 
+            }
+    
+        })  
+    })
+}
+
 function lancerJeu(){  
-    affichage_dynamique_liste.innerHTML = listes_mots[index_mots] 
-
-    bouton_validation.addEventListener('click', ()=>{
-        if (mot_utilisateur.value === affichage_dynamique_liste.innerHTML){
-            score++
-            score_change.textContent = score 
-            list_change.textContent = listes_mots.length
-            index_mots++ 
-            if (index_mots < listes_mots.length) {
-                affichage_dynamique_liste.innerHTML = listes_mots[index_mots];
-            } else {
-                affichage_dynamique_liste.innerHTML = "Le jeu est fini";
-                mot_utilisateur.disabled = true;
-            }
-            mot_utilisateur.value = ""
-        }
-
-        else{
-            index_mots++ 
-            if (index_mots < listes_mots.length) {
-                affichage_dynamique_liste.innerHTML = listes_mots[index_mots];
-            } else {
-                affichage_dynamique_liste.innerHTML = "Le jeu est fini";
-                mot_utilisateur.disabled = true;
-            }
-            mot_utilisateur.value = "" 
-        }
-
-
-
-    }) 
-
+    liste_mots.addEventListener('click', ()=>{
+    listeChoix(listes_mots, index_mots)
+    })
       
 }
 
