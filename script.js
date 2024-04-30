365711336d8692d5ecf23e0f990acda0c3c3e58f
dalilahannouche <dalilahannouche@hotmail.fr>
2024-04-30 23:29:24 +0300

    reprendre la fonction mot

--- script.js
+++ script.js
@@ -7,38 +7,37 @@ const mot_utilisateur = document.querySelector(".validation input")
 const bouton_validation = document.querySelector(".validate")
 let score_change = document.querySelector(".score")
 let list_change = document.querySelector(".liste")
-let index_list = 0
+let index_mot = 0
 let score = 0
 
 
-function lancerJeu(){  
-    affichage_dynamique_liste.innerHTML = listes_mots[index_mots] 
-    console.log(affichage_dynamique_liste.innerHTML)
+function lancerJeu() {  
+    affichage_dynamique_liste.innerHTML = listes_mots[index_mots];
 
-    bouton_validation.addEventListener('click', ()=>{
-        if (mot_utilisateur.value === affichage_dynamique_liste.innerHTML){
-            score++
-            mot_utilisateur.value = " "
-            score_change.textContent = score 
-            list_change.textContent = listes_mots.length
-            index_mots++ 
-            affichage_dynamique_liste.innerHTML = listes_mots[index_mots] 
-
-            if(index_mot === listes_mots.length){
-                affichage_dynamique_liste.innerHTML = "Le jeu est fini"
+    bouton_validation.addEventListener('click', () => {
+        if (index_mots < listes_mots.length) {
+            if (mot_utilisateur.value === affichage_dynamique_liste.innerHTML) {
+                score++;
+                score_change.textContent = score;
+                list_change.textContent = listes_mots.length;
+                index_mots++;
+                if (index_mots < listes_mots.length) {
+                    affichage_dynamique_liste.innerHTML = listes_mots[index_mots];
+                } else {
+                    affichage_dynamique_liste.innerHTML = "Le jeu est fini";
+                    mot_utilisateur.disabled = true; // Désactiver l'input
+                }
+                mot_utilisateur.value = "";
+            } else {
+                index_mots++;
+                if (index_mots < listes_mots.length) {
+                    affichage_dynamique_liste.innerHTML = listes_mots[index_mots];
+                } else {
+                    affichage_dynamique_liste.innerHTML = "Le jeu est fini";
+                    mot_utilisateur.disabled = true; // Désactiver l'input
+                }
+                mot_utilisateur.value = ""; 
             }
         }
-        else{
-            score = 0
-            list_change.textContent = listes_mots.length
-            index_mots++ 
-            affichage_dynamique_liste.innerHTML = listes_mots[index_mots]
-            mot_utilisateur.value = " " 
-        }
-    }) 
-      
+    });
 }
-
-
-listeChoix(listes_mots, index_list)
-
